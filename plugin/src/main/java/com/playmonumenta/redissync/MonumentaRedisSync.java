bbce74065191f064a8af59192572c4b01a9e31c6
Byron Marohn <combustible@live.com>
2020-03-28 12:12:08 -0700

    Initial commit
    Signed-off-by: Byron Marohn <combustible@live.com>

---
+++ plugin/src/main/java/com/playmonumenta/redissync/MonumentaRedisSync.java
@@ -0,0 +1,39 @@
+package com.playmonumenta.redissync;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+import redis.clients.jedis.JedisPool;
+import redis.clients.jedis.JedisPoolConfig;
+
+public class MonumentaRedisSync extends JavaPlugin {
+	public static JedisPool pool;
+	private static MonumentaRedisSync INSTANCE = null;
+
+	public static MonumentaRedisSync getInstance() {
+		return INSTANCE;
+	}
+
+	@Override
+	public void onLoad() {
+		/*
+		 * CommandAPI commands which register directly and are usable in functions
+		 *
+		 * These need to register immediately on load to prevent function loading errors
+		 */
+	}
+
+	//  Logic that is performed upon enabling the plugin.
+	@Override
+	public void onEnable() {
+		pool = new JedisPool(new JedisPoolConfig(), "redis", 6379);
+		INSTANCE = this;
+	}
+
+	//  Logic that is performed upon disabling the plugin.
+	@Override
+	public void onDisable() {
+		pool.close();
+		INSTANCE = null;
+		getServer().getScheduler().cancelTasks(this);
+	}
+}
