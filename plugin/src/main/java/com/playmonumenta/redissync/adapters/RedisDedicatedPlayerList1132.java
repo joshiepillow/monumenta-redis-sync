8217dd996d5118c111650e7daaa0fd0eaccc90bf
Byron Marohn <combustible@live.com>
2020-03-30 18:53:45 -0700

    Dead end prototype overloading advancements
    Signed-off-by: Byron Marohn <combustible@live.com>

--- plugin/src/main/java/com/playmonumenta/redissync/adapters/RedisDedicatedPlayerList1132.java
+++ plugin/src/main/java/com/playmonumenta/redissync/adapters/RedisDedicatedPlayerList1132.java
@@ -1,6 +1,8 @@
 package com.playmonumenta.redissync.adapters;
 
+import java.io.File;
 import java.lang.reflect.Field;
+import java.util.UUID;
 import java.util.logging.Logger;
 
 import javax.annotation.Nullable;
@@ -9,14 +11,19 @@ import org.bukkit.Bukkit;
 import org.bukkit.craftbukkit.v1_13_R2.CraftServer;
 import org.bukkit.event.Listener;
 
+import net.minecraft.server.v1_13_R2.AdvancementDataPlayer;
 import net.minecraft.server.v1_13_R2.DedicatedPlayerList;
 import net.minecraft.server.v1_13_R2.DedicatedServer;
+import net.minecraft.server.v1_13_R2.DimensionManager;
 import net.minecraft.server.v1_13_R2.EntityPlayer;
+import net.minecraft.server.v1_13_R2.MinecraftServer;
 import net.minecraft.server.v1_13_R2.NBTTagCompound;
+import net.minecraft.server.v1_13_R2.ServerStatisticManager;
 
 public class RedisDedicatedPlayerList1132 extends DedicatedPlayerList implements Listener {
 	private static RedisDedicatedPlayerList1132 INSTANCE = null;
 	private final Logger mLog;
+	private final MinecraftServer mServer;
 
 	public static RedisDedicatedPlayerList1132 getInstance() {
 		return INSTANCE;
@@ -46,12 +53,26 @@ public class RedisDedicatedPlayerList1132 extends DedicatedPlayerList implements
 	private RedisDedicatedPlayerList1132(DedicatedPlayerList list, Logger logger) {
 		super(list);
 		mLog = logger;
+		mServer = list.getServer();
     }
 
 	@Override
-	public void savePlayerFile(EntityPlayer player) {
-		super.savePlayerFile(player);
-		mLog.info("Saving player data: " + player.getUniqueID().toString());
+	public void savePlayerFile(EntityPlayer entityplayer) {
+		mLog.info("Saving player data: " + entityplayer.getUniqueID().toString());
+
+        entityplayer.lastSave = MinecraftServer.currentTick; // Paper
+        this.playerFileData.save(entityplayer);
+        ServerStatisticManager serverstatisticmanager = (ServerStatisticManager) entityplayer.getStatisticManager(); // CraftBukkit
+
+        if (serverstatisticmanager != null) {
+            serverstatisticmanager.a();
+        }
+
+        AdvancementDataPlayer advancementdataplayer = (AdvancementDataPlayer) entityplayer.getAdvancementData(); // CraftBukkit
+
+        if (advancementdataplayer != null) {
+            advancementdataplayer.c();
+        }
 	}
 
 	@Nullable
@@ -59,5 +80,19 @@ public class RedisDedicatedPlayerList1132 extends DedicatedPlayerList implements
     public NBTTagCompound a(EntityPlayer player) {
 		mLog.info("Loading player data: " + player.getUniqueID().toString());
 		return super.a(player);
+    }
+
+    public AdvancementDataPlayer h(EntityPlayer entityplayer) {
+        AdvancementDataPlayer advancementdataplayer = entityplayer.getAdvancementData();
+
+        if (advancementdataplayer == null) {
+			UUID uuid = entityplayer.getUniqueID();
+            File file = new File(this.mServer.getWorldServer(DimensionManager.OVERWORLD).getDataManager().getDirectory(), "advancements");
+            File file1 = new File(file, uuid + ".json");
+
+            advancementdataplayer = new AdvancementDataPlayer(this.server, file1, entityplayer);
+        }
+
+        return advancementdataplayer;
     }
 }
