ed66fed30c08e1a60e26fe47d400656ac30972b1
Byron Marohn <combustible@live.com>
2020-10-07 15:40:17 -0700

    Fix github actions, add build cache (#5)
    Signed-off-by: Byron Marohn <combustible@live.com>

--- compile.sh
+++ compile.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
-$SCRIPT_DIR/mvn_version_wrapper.sh install
+$SCRIPT_DIR/mvn_version_wrapper.sh install "$@"
--- deploy.sh
+++ deploy.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
-$SCRIPT_DIR/mvn_version_wrapper.sh deploy
+$SCRIPT_DIR/mvn_version_wrapper.sh deploy "$@"
