ed8fba1b3831ae647835b22d49365dfa72322313
Byron Marohn <Combustible@live.com>
2021-12-18 03:44:41 +0000

    Fix memory leak in AutoSaveListener
    Signed-off-by: Byron Marohn <Combustible@live.com>

--- plugin/src/main/java/com/playmonumenta/redissync/AutoSaveListener.java
+++ plugin/src/main/java/com/playmonumenta/redissync/AutoSaveListener.java
@@ -20,75 +20,60 @@ import org.bukkit.event.Listener;
 import org.bukkit.event.player.PlayerJoinEvent;
 import org.bukkit.event.player.PlayerQuitEvent;
 import org.bukkit.plugin.Plugin;
-import org.bukkit.scheduler.BukkitRunnable;
+import org.bukkit.scheduler.BukkitTask;
 
 public class AutoSaveListener implements Listener {
-	private final Map<UUID, BukkitRunnable> mPendingSaves = new HashMap<>();
+	private final Map<UUID, BukkitTask> mPendingSaves = new HashMap<>();
 
 	protected AutoSaveListener(Plugin plugin, VersionAdapter adapter) {
 		Logger logger = plugin.getLogger();
 
-		new BukkitRunnable() {
-			@Override
-			public void run() {
-				// Create a local copy of the online players list
-				List<? extends Player> players = new ArrayList<>(Bukkit.getOnlinePlayers());
+		Bukkit.getScheduler().runTaskTimer(plugin, () -> {
+			// Create a local copy of the online players list
+			List<? extends Player> players = new ArrayList<>(Bukkit.getOnlinePlayers());
 
-				if (players.size() <= 0) {
-					// Nothing to do this iteration
-					return;
-				}
+			if (players.size() <= 0) {
+				// Nothing to do this iteration
+				return;
+			}
 
-				// Sort the players list by name
-				// This helps ensure that a player will be saved consistently about the same time apart
-				players.sort((a, b) -> a.getName().compareTo(b.getName()));
+			// Sort the players list by name
+			// This helps ensure that a player will be saved consistently about the same time apart
+			players.sort((a, b) -> a.getName().compareTo(b.getName()));
 
-				// Distribute saves evenly in the autosave interval.
-				// This means that at most a player will take two autosave intervals to save
-				int delayBetweenSaves = Conf.getTicksPerPlayerAutosave() / players.size();
+			// Distribute saves evenly in the autosave interval.
+			// This means that at most a player will take two autosave intervals to save
+			int delayBetweenSaves = Conf.getTicksPerPlayerAutosave() / players.size();
 
-				// Remove all the previous iteration saves
-				Iterator<Map.Entry<UUID, BukkitRunnable>> iter = mPendingSaves.entrySet().iterator();
-				while (iter.hasNext()) {
-					Map.Entry<UUID, BukkitRunnable> entry = iter.next();
+			// Remove all the previous iteration saves
+			for (Map.Entry<UUID, BukkitTask> entry : mPendingSaves.entrySet()) {
+				logger.severe("Player autosave for " + entry.getKey() + " did not complete before next autosave!");
+				entry.getValue().cancel();
+			}
+			mPendingSaves.clear();
 
-					if (!entry.getValue().isCancelled()) {
-						logger.severe("Player autosave for " + entry.getKey() + " did not complete before next autosave!");
-						entry.getValue().cancel();
+			// Schedule an individual save task one-time runnable for each player
+			for (int i = 0; i < players.size(); i++) {
+				Player player = players.get(i);
+				UUID uuid = player.getUniqueId();
+
+				// Schedule this player's save for that later time slot
+				BukkitTask task = Bukkit.getScheduler().runTaskLater(plugin, () -> {
+					try {
+						if (player.isOnline() && !DataEventListener.isPlayerTransferring(player)) {
+							adapter.savePlayer(player);
+						}
+					} catch (Exception ex) {
+						logger.severe("Failed to autosave player " + player.getName() + ":" + ex.getMessage());
+						ex.printStackTrace();
 					}
 
-					iter.remove();
-				}
+					mPendingSaves.remove(uuid);
+				}, i * delayBetweenSaves);
 
-				// Schedule an individual save task one-time runnable for each player
-				for (int i = 0; i < players.size(); i++) {
-					Player player = players.get(i);
-					UUID uuid = player.getUniqueId();
-
-					BukkitRunnable saveRunnable = new BukkitRunnable() {
-						@Override
-						public void run() {
-							try {
-								if (player.isOnline() && !DataEventListener.isPlayerTransferring(player)) {
-									adapter.savePlayer(player);
-								}
-							} catch (Exception ex) {
-								logger.severe("Failed to autosave player " + player.getName() + ":" + ex.getMessage());
-								ex.printStackTrace();
-							}
-
-							// Needed to mark this task as complete
-							this.cancel();
-						}
-					};
-
-					// Schedule this player's save for that later time slot
-					saveRunnable.runTaskLater(plugin, i * delayBetweenSaves);
-
-					mPendingSaves.put(uuid, saveRunnable);
-				}
+				mPendingSaves.put(uuid, task);
 			}
-		}.runTaskTimer(plugin, Conf.getTicksPerPlayerAutosave(), Conf.getTicksPerPlayerAutosave());
+		}, Conf.getTicksPerPlayerAutosave(), Conf.getTicksPerPlayerAutosave());
 	}
 
 	@EventHandler(priority = EventPriority.HIGHEST)
@@ -112,7 +97,7 @@ public class AutoSaveListener implements Listener {
 	}
 
 	private void cancelSaveTask(Player player) {
-		BukkitRunnable cleanupTask = mPendingSaves.remove(player.getUniqueId());
+		BukkitTask cleanupTask = mPendingSaves.remove(player.getUniqueId());
 		if (cleanupTask != null && !cleanupTask.isCancelled()) {
 			cleanupTask.cancel();
 		}
