d9a848682a83d1b2b3172317eb62318a438e67d4
Nobody314159 <84757662+Nobody314159@users.noreply.github.com>
2022-11-11 23:39:55 -0800

    Added simpler set and getAsLong methods in RBoardAPI, clean up exception handling (#36)
    Co-authored-by: Byron Marohn <Combustible@live.com>

--- plugin/src/main/java/com/playmonumenta/redissync/RBoardAPI.java
+++ plugin/src/main/java/com/playmonumenta/redissync/RBoardAPI.java
@@ -1,5 +1,6 @@
 package com.playmonumenta.redissync;
 
+import java.util.HashMap;
 import java.util.LinkedHashMap;
 import java.util.List;
 import java.util.Map;
@@ -9,33 +10,66 @@ import io.lettuce.core.api.async.RedisAsyncCommands;
 
 public class RBoardAPI {
 
-	public static String getRedisPath(String name) throws Exception {
+	public static String getRedisPath(String name) throws IllegalArgumentException {
 		if (!name.matches("^[-_0-9A-Za-z$]+$")) {
-			throw new Exception("Name '" + name + "' contains illegal characters, must match '^[-_$0-9A-Za-z$]+'");
+			throw new IllegalArgumentException("Name '" + name + "' contains illegal characters, must match '^[-_$0-9A-Za-z$]+'");
 		}
 		return String.format("%s:rboard:%s", ConfigAPI.getServerDomain(), name);
 	}
 
 	/********************* Set *********************/
-	public static CompletableFuture<Long> set(String name, Map<String, String> data) throws Exception {
+	public static CompletableFuture<Long> set(String name, Map<String, String> data) {
+		final String redisPath;
+		try {
+			redisPath = getRedisPath(name);
+		} catch (IllegalArgumentException ex) {
+			CompletableFuture<Long> future = new CompletableFuture<>();
+			future.completeExceptionally(ex);
+			return future;
+		}
+
 		RedisAsyncCommands<String, String> commands = RedisAPI.getInstance().async();
-		return commands.hset(getRedisPath(name), data).toCompletableFuture();
+		return commands.hset(redisPath, data).toCompletableFuture();
+	}
+
+	public static CompletableFuture<Long> set(String name, String key, long amount) {
+		Map<String, String> data = new HashMap<>();
+		data.put(key, Long.toString(amount));
+		return set(name, data);
 	}
 
 	/********************* Add *********************/
-	public static CompletableFuture<Long> add(String name, String key, long amount) throws Exception {
+	public static CompletableFuture<Long> add(String name, String key, long amount) {
+		final String redisPath;
+		try {
+			redisPath = getRedisPath(name);
+		} catch (IllegalArgumentException ex) {
+			CompletableFuture<Long> future = new CompletableFuture<>();
+			future.completeExceptionally(ex);
+			return future;
+		}
+
 		RedisAsyncCommands<String, String> commands = RedisAPI.getInstance().async();
-		return commands.hincrby(getRedisPath(name), key, amount).toCompletableFuture();
+		return commands.hincrby(redisPath, key, amount).toCompletableFuture();
 	}
 
 	/********************* Get *********************/
-	public static CompletableFuture<Map<String, String>> get(String name, String... keys) throws Exception {
+	public static CompletableFuture<Map<String, String>> get(String name, String... keys) {
+		final String redisPath;
+		try {
+			redisPath = getRedisPath(name);
+		} catch (IllegalArgumentException ex) {
+			CompletableFuture<Map<String, String>> future = new CompletableFuture<>();
+			future.completeExceptionally(ex);
+			return future;
+		}
+
 		RedisAsyncCommands<String, String> commands = RedisAPI.getInstance().async();
 		commands.multi();
 		for (String key : keys) {
-			commands.hincrby(getRedisPath(name), key, 0);
+			commands.hincrby(redisPath, key, 0);
 		}
-		CompletableFuture<Map<String, String>> retval = commands.hmget(getRedisPath(name), keys).toCompletableFuture().thenApply(list -> {
+		CompletableFuture<Map<String, String>> retval = commands.hmget(redisPath, keys).toCompletableFuture().thenApply(list -> {
 			Map<String, String> transformed = new LinkedHashMap<>();
 			list.forEach(item -> transformed.put(item.getKey(), item.getValue()));
 			return transformed;
@@ -44,41 +78,97 @@ public class RBoardAPI {
 		return retval;
 	}
 
+	public static CompletableFuture<Long> getAsLong(String name, String key, long def) {
+		return get(name, key).thenApply(data -> {
+			String value = data.get(key);
+			if (value != null) {
+				/* Note this may throw a NumberFormatException, but caller already should catch exceptional completion */
+				return Long.parseLong(value);
+			}
+			return def;
+		});
+	}
+
 	/********************* GetAndReset *********************/
-	public static CompletableFuture<Map<String, String>> getAndReset(String name, String... keys) throws Exception {
+	public static CompletableFuture<Map<String, String>> getAndReset(String name, String... keys) {
+		final String redisPath;
+		try {
+			redisPath = getRedisPath(name);
+		} catch (IllegalArgumentException ex) {
+			CompletableFuture<Map<String, String>> future = new CompletableFuture<>();
+			future.completeExceptionally(ex);
+			return future;
+		}
+
 		RedisAsyncCommands<String, String> commands = RedisAPI.getInstance().async();
 		commands.multi();
-		CompletableFuture<Map<String, String>> retval = commands.hmget(getRedisPath(name), keys).toCompletableFuture().thenApply(list -> {
+		CompletableFuture<Map<String, String>> retval = commands.hmget(redisPath, keys).toCompletableFuture().thenApply(list -> {
 			Map<String, String> transformed = new LinkedHashMap<>();
 			list.forEach(item -> transformed.put(item.getKey(), item.getValue()));
 			return transformed;
 		});
-		commands.hdel(getRedisPath(name), keys).toCompletableFuture();
+		commands.hdel(redisPath, keys).toCompletableFuture();
 		commands.exec();
 		return retval;
 	}
 
 	/********************* GetKeys *********************/
-	public static CompletableFuture<List<String>> getKeys(String name) throws Exception {
+	public static CompletableFuture<List<String>> getKeys(String name) {
+		final String redisPath;
+		try {
+			redisPath = getRedisPath(name);
+		} catch (IllegalArgumentException ex) {
+			CompletableFuture<List<String>> future = new CompletableFuture<>();
+			future.completeExceptionally(ex);
+			return future;
+		}
+
 		RedisAsyncCommands<String, String> commands = RedisAPI.getInstance().async();
-		return commands.hkeys(getRedisPath(name)).toCompletableFuture();
+		return commands.hkeys(redisPath).toCompletableFuture();
 	}
 
 	/********************* GetAll *********************/
-	public static CompletableFuture<Map<String, String>> getAll(String name) throws Exception {
+	public static CompletableFuture<Map<String, String>> getAll(String name) {
+		final String redisPath;
+		try {
+			redisPath = getRedisPath(name);
+		} catch (IllegalArgumentException ex) {
+			CompletableFuture<Map<String, String>> future = new CompletableFuture<>();
+			future.completeExceptionally(ex);
+			return future;
+		}
+
 		RedisAsyncCommands<String, String> commands = RedisAPI.getInstance().async();
-		return commands.hgetall(getRedisPath(name)).toCompletableFuture();
+		return commands.hgetall(redisPath).toCompletableFuture();
 	}
 
 	/********************* Reset *********************/
-	public static CompletableFuture<Long> reset(String name, String... keys) throws Exception {
+	public static CompletableFuture<Long> reset(String name, String... keys) {
+		final String redisPath;
+		try {
+			redisPath = getRedisPath(name);
+		} catch (IllegalArgumentException ex) {
+			CompletableFuture<Long> future = new CompletableFuture<>();
+			future.completeExceptionally(ex);
+			return future;
+		}
+
 		RedisAsyncCommands<String, String> commands = RedisAPI.getInstance().async();
-		return commands.hdel(getRedisPath(name), keys).toCompletableFuture();
+		return commands.hdel(redisPath, keys).toCompletableFuture();
 	}
 
 	/********************* ResetAll *********************/
-	public static CompletableFuture<Long> resetAll(String name) throws Exception {
+	public static CompletableFuture<Long> resetAll(String name) {
+		final String redisPath;
+		try {
+			redisPath = getRedisPath(name);
+		} catch (IllegalArgumentException ex) {
+			CompletableFuture<Long> future = new CompletableFuture<>();
+			future.completeExceptionally(ex);
+			return future;
+		}
+
 		RedisAsyncCommands<String, String> commands = RedisAPI.getInstance().async();
-		return commands.del(getRedisPath(name)).toCompletableFuture();
+		return commands.del(redisPath).toCompletableFuture();
 	}
 }
