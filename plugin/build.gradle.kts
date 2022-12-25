e19b0ab7aff842b479001eaafc1c513ff7fcab34
Byron Marohn <Combustible@live.com>
2022-12-25 04:56:11 +0000

    Add 1.19.3 support (v1_19_R3)
    Signed-off-by: Byron Marohn <Combustible@live.com>

--- plugin/build.gradle.kts
+++ plugin/build.gradle.kts
@@ -21,6 +21,7 @@ plugins {
 dependencies {
     implementation(project(":adapter_api"))
     implementation(project(":adapter_v1_18_R2", "reobf"))
+    implementation(project(":adapter_v1_19_R2", "reobf"))
     implementation("io.lettuce:lettuce-core:5.3.5.RELEASE")
     compileOnly("net.md-5:bungeecord-api:1.15-SNAPSHOT")
     compileOnly("com.playmonumenta:monumenta-network-relay:1.0")
--- settings.gradle.kts
+++ settings.gradle.kts
@@ -2,6 +2,7 @@ rootProject.name = "monumenta-redis-sync"
 include(":adapter_api")
 include(":adapter_v1_16_R3")
 include(":adapter_v1_18_R2")
+include(":adapter_v1_19_R2")
 include(":redissync-example")
 include(":redissync")
 project(":redissync-example").projectDir = file("example")
